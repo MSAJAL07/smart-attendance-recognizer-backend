21c6b833ae5d4b70a5366218385f9ded75126bbc
ambarish99 <ambarishdashora87@gmail.com>
2021-03-29 23:25:06 +0530

    added modules

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 console.log("Jai Shree Ganesh")
+const express=require('express');
+const bodyParser=require('body-parser');
+
+const envFile=require('dotenv');
+envFile.config();
+
+const config=require('./config/config');
+const routes=require('./routes/router');
+const db=require('./config/db');
+
+var app=express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+   extended:true
+}));
+app.use('/api/v1/',routes);
+
+db.on('connected',()=>console.log("Succesfully connected to database!!"));
+app.listen(config.server.port,()=>{console.log("Server started and listening on port:",config.server.port)});
+
