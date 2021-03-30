d698052358868e413a868605c0ce60af1de1845b
ambarish99 <ambarishdashora87@gmail.com>
2021-03-30 18:36:21 +0530

    constants

---
+++ constants/regex.js
@@ -0,0 +1,8 @@
+module.exports = Object.freeze({
+   //nameRegex: /^[a-zA-Z]+$/,
+   nameRegex: /^[\w -]+( \w+)*$/,
+   phoneRegex: /\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/,
+   usernameRegex: /\S+/,
+   passwordRegex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,20}$/,
+   petNameRegex: /^[a-zA-Z ]+$/
+});
