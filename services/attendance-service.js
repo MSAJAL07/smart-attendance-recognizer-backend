bf45001e132f3e958f5f31ad85cb9bf1c6d38ed3
ambarish99 <ambarishdashora87@gmail.com>
2021-06-02 11:26:49 +0530

    api for get attendance

--- services/attendance-service.js
+++ services/attendance-service.js
@@ -13,20 +13,27 @@ module.exports.processImages=async (data,files)=>{
 module.exports.getAllAttendance=(req)=>{
    const fdir=path.join(__dirname,'../','faceRecognizer/Attendance');
    var result=[];
+   var oneFile;
    fs.readdirSync(fdir).forEach((file)=>{
       var data=fs.readFileSync(path.join(__dirname,'../','faceRecognizer/Attendance',file),'utf-8');
       var chunks=data.split('\r\n');
+      oneFile=[];
       for(var i=1;i<chunks.length-1;i++)
       {
          var row=chunks[i].split(',');
          var id=row[0];
          var name=row[1];
+         var date=row[2];
+         var time=row[3];
          console.log(id,name);
          var doc={};
          doc.id=id;
          doc.name=name;
-         result.push(doc);
+         doc.date=date;
+         doc.time=time;
+         oneFile.push(doc);
       }
+      result.push(oneFile);
    });
    console.log(result);
    return  {
